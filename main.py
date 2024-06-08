038afc9b7a7900a3203a1742ad7354109dc3584c
Ariel Liu <cutiepanda2468@gmail.com>
2024-06-07 21:19:44 -0400

    script

--- main.py
+++ main.py
@@ -21,5 +21,4 @@ print(f"Summary: {article.summary}")
 
 analysis = TextBlob(article.text)
 
-print(analysis.polarity)
 print(f"Sentiment: {'positive' if analysis.polarity > 0 else 'negative' if analysis.polarity < 0 else 'neutral'}")
