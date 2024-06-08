5fff1aebe4b398ff5852fece696e38ae225978cd
Ariel Liu <cutiepanda2468@gmail.com>
2024-06-07 21:16:19 -0400

    imports and initial stuff

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 import tkinter as tk
 import nltk
 from textblob import TextBlob
-from newspaper import article
+from newspaper import Article
+
+nltk.download("punkt")
+
+url = "https://www.rollingstone.com/music/music-news/bts-jung-kook-never-let-go-single-1235034912/"
+
+article = Article(url)
+
+article.download()
+article.parse()
+
+article.nlp()
+
+print(f"Title: {article.title}")
+print(f"Authors: {article.authors}")
+print(f"Publication Date: {article.publish_date}")
+print(f"Summary: {article.summary}")
+
+analysis = TextBlob(article.text)
+
+print(analysis.polarity)
+print(f"Sentiment: {"positive" if analysis.polarity > 0 else "negative" if analysis.polarity < 0 else "neutral"}")
